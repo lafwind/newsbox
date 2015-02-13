a75d75ad213918c9a25de7e11c538180557178e4
Lafwind <lafwind@gmail.com>
2015-02-14 03:12:20 +0800

    修改tab样式及行为

--- www/js/app.js
+++ www/js/app.js
@@ -3,37 +3,62 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var App = angular.module('starter', ['ionic'])
+var App = angular.module('LafApp', ['ionic'])
 
-App.controller("AppCtrl", ["$scope", "$log", AppCtrl]);
-
-// .run(function($ionicPlatform) {
-//   $ionicPlatform.ready(function() {
-//     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//     // for form inputs)
-//     if(window.cordova && window.cordova.plugins.Keyboard) {
-//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//     }
-//     if(window.StatusBar) {
-//       StatusBar.styleDefault();
-//     }
-//   });
+// App.controller('RootCtrl', function($scope) {
+//   $scope.onControllerChanged = function(oldController, oldIndex, newController, newIndex) {
+//     console.log('Controller changed', oldController, oldIndex, newController, newIndex);
+//     console.log(arguments);
+//   };
 // })
 
-function AppCtrl($scope, $log) {
+// App.controller('HomeCtrl', function($scope, $timeout, $ionicModal, $ionicActionSheet) {
+//
+//   $scope.onRefresh = function() {
+//     console.log('ON REFRESH');
+//
+//     $timeout(function() {
+//       $scope.$broadcast('scroll.refreshComplete');
+//     }, 1000);
+//   }
+//
+// })
+
+App.controller("HomeCtrl", ["$scope", "$log", HomeCtrl]);
+App.controller("LikeCtrl", ["$scope", "$log", LikeCtrl]);
+App.controller("LetterCtrl", ["$scope", "$log", LetterCtrl]);
+
+App.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+function HomeCtrl($scope, $log) {
   $scope.refresh = function() {
-    alert("别逗～小箫歌～");
+    alert("别逗了～不会动的～小箫歌～");
+  }
+}
+
+function LikeCtrl($scope, $log) {
+  $scope.refresh = function() {
+    alert("别逗了～不会动的～小箫歌～");
+  }
+}
+
+function LetterCtrl($scope, $log) {
+  $scope.refresh = function() {
+    alert("别逗了～不会动的～小箫歌～");
   }
 
-  $scope.home = function() {
-    alert("还逗~大哥~真没啥~");
-  }
-
-  $scope.like = function() {
-    alert("虽然你这么逗~但我还是喜欢你！");
-  }
-
-  $scope.setting = function() {
-    alert("真的别逗了哦小淘气～嘿嘿~");
+  $scope.mention = function() {
+    alert("有彩蛋！");
   }
 }
